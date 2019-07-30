8e53161adde27d88828db8a3c8ac0e2e8b755b8e
apshu <attila.kolinger@gmail.com>
2019-07-30 18:53:55 +0200

    Build info changed to reflect current status

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -23,7 +23,7 @@
 
 // Grbl versioning system
 #define GRBL_VERSION "1.1g"
-#define GRBL_VERSION_BUILD "20181112"
+#define GRBL_VERSION_BUILD "20190730"
 
 // Define standard libraries used by Grbl.
 #include <xc.h>
