ec8a76a5267275494fc9f982d36f33a19961f21f
apshu <attila.kolinger@gmail.com>
2019-08-03 01:05:50 +0200

    Emulating EEPRIM in MCU RAM

--- grbl/eeprom.c
+++ grbl/eeprom.c
@@ -23,18 +23,8 @@
 ****************************************************************************/
 #include "grbl.h"
 
-/* These EEPROM bits have different names on different devices. */
-#ifndef EEPE
-		#define EEPE  EEWE  //!< EEPROM program/write enable.
-		#define EEMPE EEMWE //!< EEPROM master program/write enable.
-#endif
-
-/* These two are unfortunately not defined in the device include files. */
-#define EEPM1 5 //!< EEPROM Programming Mode Bit 1.
-#define EEPM0 4 //!< EEPROM Programming Mode Bit 0.
-
-/* Define to reduce code size. */
-#define EEPROM_IGNORE_SELFPROG //!< Remove SPM flag polling.
+static uint8_t EEPROM_emuBuffer[2046];
+static bool isEEP_firstAccess = true;
 
 /*! \brief  Read byte from EEPROM.
  *
@@ -47,11 +37,14 @@
  */
 unsigned char eeprom_get_char( unsigned int addr )
 {
-//	do {} while( EECR & (1<<EEPE) ); // Wait for completion of previous write.
-//	EEAR = addr; // Set EEPROM address register.
-//	EECR = (1<<EERE); // Start EEPROM read operation.
-//	return EEDR; // Return the byte read from EEPROM.
-    return 1;
+    if (isEEP_firstAccess) {
+        memset(EEPROM_emuBuffer, 0xFF, sizeof(EEPROM_emuBuffer));
+        isEEP_firstAccess = false;
+    }
+    if (addr < sizeof(EEPROM_emuBuffer)) {
+        return EEPROM_emuBuffer[addr];
+    }
+    return 0xFF;
 }
 
 /*! \brief  Write byte to EEPROM.
@@ -73,55 +66,13 @@ unsigned char eeprom_get_char( unsigned int addr )
  */
 void eeprom_put_char( unsigned int addr, unsigned char new_value )
 {
-//	char old_value; // Old EEPROM value.
-//	char diff_mask; // Difference mask, i.e. old value XOR new value.
-//
-//	cli(); // Ensure atomic operation for the write operation.
-//	
-//	do {} while( EECR & (1<<EEPE) ); // Wait for completion of previous write.
-//	#ifndef EEPROM_IGNORE_SELFPROG
-//	do {} while( SPMCSR & (1<<SELFPRGEN) ); // Wait for completion of SPM.
-//	#endif
-//	
-//	EEAR = addr; // Set EEPROM address register.
-//	EECR = (1<<EERE); // Start EEPROM read operation.
-//	old_value = EEDR; // Get old EEPROM value.
-//	diff_mask = old_value ^ new_value; // Get bit differences.
-//	
-//	// Check if any bits are changed to '1' in the new value.
-//	if( diff_mask & new_value ) {
-//		// Now we know that _some_ bits need to be erased to '1'.
-//		
-//		// Check if any bits in the new value are '0'.
-//		if( new_value != 0xff ) {
-//			// Now we know that some bits need to be programmed to '0' also.
-//			
-//			EEDR = new_value; // Set EEPROM data register.
-//			EECR = (1<<EEMPE) | // Set Master Write Enable bit...
-//			       (0<<EEPM1) | (0<<EEPM0); // ...and Erase+Write mode.
-//			EECR |= (1<<EEPE);  // Start Erase+Write operation.
-//		} else {
-//			// Now we know that all bits should be erased.
-//
-//			EECR = (1<<EEMPE) | // Set Master Write Enable bit...
-//			       (1<<EEPM0);  // ...and Erase-only mode.
-//			EECR |= (1<<EEPE);  // Start Erase-only operation.
-//		}
-//	} else {
-//		// Now we know that _no_ bits need to be erased to '1'.
-//		
-//		// Check if any bits are changed from '1' in the old value.
-//		if( diff_mask ) {
-//			// Now we know that _some_ bits need to the programmed to '0'.
-//			
-//			EEDR = new_value;   // Set EEPROM data register.
-//			EECR = (1<<EEMPE) | // Set Master Write Enable bit...
-//			       (1<<EEPM1);  // ...and Write-only mode.
-//			EECR |= (1<<EEPE);  // Start Write-only operation.
-//		}
-//	}
-//	
-//	sei(); // Restore interrupt flag state.
+    if (isEEP_firstAccess) {
+        memset(EEPROM_emuBuffer, 0xFF, sizeof(EEPROM_emuBuffer));
+        isEEP_firstAccess = false;
+    }
+    if (addr < sizeof(EEPROM_emuBuffer)) {
+        EEPROM_emuBuffer[addr] = new_value;
+    }
 }
 
 // Extensions added as part of Grbl 
