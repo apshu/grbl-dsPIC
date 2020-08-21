e572d33d93fc768a04e4f32abc306e1999679b17
apshu <attila.kolinger@gmail.com>
2020-08-21 09:25:39 +0200

    Project renamed to grbl-DualCore

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=grbl-dsPIC.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/grbl-dsPIC.production.hex
+CND_ARTIFACT_NAME_default=grbl-DualCore.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/grbl-DualCore.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=grbl-dspic.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/grbl-dspic.tar
+CND_PACKAGE_NAME_default=grbl-dualcore.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/grbl-dualcore.tar
--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/grbl-dsPIC.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=grbl-dsPIC.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=grbl-dspic/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/grbl-DualCore.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=grbl-DualCore.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=grbl-dualcore/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/grbl-dspic/bin
+makeDirectory ${TMPDIR}/grbl-dualcore/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/grbl-dspic.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/grbl-dualcore.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/grbl-dspic.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/grbl-dualcore.tar *
 checkReturnCode
 
 # Cleanup
